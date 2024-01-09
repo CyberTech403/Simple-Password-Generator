63f680a327f8fe4d7a89315cc1a4ef45802ce0db
Phisit  Jansopha <146698658+CyberTech403@users.noreply.github.com>
2024-01-09 20:11:21 +0000

    amended script JS

--- script.js
+++ script.js
@@ -1,119 +1,35 @@
-// Array of special characters to be included in password
+const passwordGenerateBox = document.getElementById("password");
 
-var specialCharacters = [
-  '@',
-  '%',
-  '+',
-  '\\',
-  '/',
-  "'",
-  '!',
-  '#',
-  '$',
-  '^',
-  '?',
-  ':',
-  ',',
-  ')',
-  '(',
-  '}',
-  '{',
-  ']',
-  '[',
-  '~',
-  '-',
-  '_',
-  '.'
-];
-
-// Array of numeric characters to be included in password
-var numericCharacters = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
-
-// Array of lowercase characters to be included in password
-var lowerCasedCharacters = [
-  'a',
-  'b',
-  'c',
-  'd',
-  'e',
-  'f',
-  'g',
-  'h',
-  'i',
-  'j',
-  'k',
-  'l',
-  'm',
-  'n',
-  'o',
-  'p',
-  'q',
-  'r',
-  's',
-  't',
-  'u',
-  'v',
-  'w',
-  'x',
-  'y',
-  'z'
-];
-
-// Array of uppercase characters to be included in password
-var upperCasedCharacters = [
-  'A',
-  'B',
-  'C',
-  'D',
-  'E',
-  'F',
-  'G',
-  'H',
-  'I',
-  'J',
-  'K',
-  'L',
-  'M',
-  'N',
-  'O',
-  'P',
-  'Q',
-  'R',
-  'S',
-  'T',
-  'U',
-  'V',
-  'W',
-  'X',
-  'Y',
-  'Z'
-];
-
-// Function to prompt user for password options
-function getPasswordOptions() {
-
-}
-
-// Function for getting a random element from an array
-function getRandom(arr) {
-
-}
-
-// Function to generate password with user input
 function generatePassword() {
+  const length = parseInt(document.getElementById("length").value);
+  const addLowercase = document.getElementById("lowercase").checked;
+  const addUppercase = document.getElementById("uppercase").checked;
+  const addNumbers = document.getElementById("numbers").checked;
+  const addSymbols = document.getElementById("symbols").checked;
 
+  const chars = [
+    "abcdefghijklmnopqrstuvwxyz",
+    "ABCDEFGHIGKLMNOPQRSTUVWXYZ",
+    "0123456789",
+    "@%+/'!#$^?:,)(}{][~-_."
+  ];
+
+  const allowedChars = [addLowercase, addUppercase, addNumbers, addSymbols]
+    .map((set, index) => (set ? chars[index] : ""))
+    .join("");
+
+  if (length < 8 || allowedChars.length === 0) {
+    const message = length < 8 ? 
+    "(Password must be at least 8 characters)" 
+    : 
+    "(At least 1 set of parameters must be selected)";
+    alert(message);
+   
+    return;
+  }
+
+  const password = Array.from({ length }, () => allowedChars[Math.floor(Math.random() * allowedChars.length)]).join("");
+  passwordGenerateBox.textContent = password;
+  
+  return password;
 }
-
-// Get references to the #generate element
-var generateBtn = document.querySelector('#generate');
-
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector('#password');
-
-  passwordText.value = password;
-}
-
-// Add event listener to generate button
-generateBtn.addEventListener('click', writePassword);
